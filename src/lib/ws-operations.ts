77db9747a3fc6f293aa914fbbb89dcd292cbf29a
MaxE688 <max.evers8@gmail.com>
2024-09-19 12:43:10 -0400

    Isolate socket code

--- src/lib/ws-operations.ts
+++ src/lib/ws-operations.ts
@@ -1,20 +1,30 @@
+import { useEffect, useRef } from "react";
 import { dow30, TradeInfo } from "./definitions";
 
 let unprocessedMessages: Array<any> = [];
 // let processedMessages: Array<any> = [];
 
 
+// let socket: WebSocket;
+let send: (data: string | ArrayBufferLike | Blob | ArrayBufferView) => void;
+export const setSend = (s: (data: string | ArrayBufferLike | Blob | ArrayBufferView) => void) => {
+  send = s;
+}
 
-export const subscribe = (stocks: string[], send: (data: string | ArrayBufferLike | Blob | ArrayBufferView) => void) => {
+
+
+export const subscribe = (stocks: string[]) => {
 // export const subscribe = (stocks: string[], socket: WebSocket) => {
   // while(socket.readyState === socket.CONNECTING){}
   stocks.forEach( ( symbol ) => {
     
-    send(JSON.stringify({
+    const res = send(JSON.stringify({
     // socket.send(JSON.stringify({
       'type':'subscribe',
       'symbol':symbol
     }));
+
+    console.log(res);
   });
 }
 
@@ -25,6 +35,7 @@ export const startTick = (setTrades: (t: Array<TradeInfo>) => void) => {
 
       unprocessedMessages.forEach((item) => {
         if(symbols.findIndex((element) => element.s === item.s) < 0){
+          // if(item.s === "GME") console.log("********************************\nGME FOUND!!!\n*********************************")
           symbols.push(item);
         }
       });
@@ -51,4 +62,64 @@ export const messageRecieved = (event: MessageEvent) => {
   } catch (error) {
     console.log("Handle Message Error:", error);
   }
+}
+
+export function openWebSocket ( 
+  url: string, 
+  symbols: Array<string>,
+  socketRef: React.MutableRefObject<WebSocket | null>, 
+  setTrades: (a: Array<TradeInfo>)=>void 
+){
+  // const socketRef = useRef<WebSocket | null>(null);
+
+
+  try {
+    //const socket = openWebSocket(url, dow30.keys, )
+    const socket = new WebSocket(url);
+    socketRef.current = socket;
+    
+    
+    //this is where the listeners are configured, add custom functions to get expected behavior
+    socketRef.current.onopen = () => {
+      console.log("Socket opened");
+      // setIsReady(true);
+      let abc = socketRef;
+      subscribe(symbols);
+      startTick(setTrades);
+      
+    };
+    socketRef.current.onclose = () => {
+    // setIsReady(false);
+    console.log("Socket closed");
+    };
+    socketRef.current.onmessage = (event) => {
+      // setValue(event.data);
+      messageRecieved(event);
+      console.log("onmessage: ");
+    };
+    
+  
+  
+    if(socketRef.current){
+      send = socketRef.current.send.bind(socketRef.current);
+    }
+    return send;
+    // return socket;
+  
+  // socketRef.current = socket;
+  // 
+  
+  
+    // return () => {
+    //   if(socketRef.current?.readyState === socket.OPEN) {
+    //     console.log("Closing socket");
+    //     // setReconnect(false);
+    //     socketRef.current.close();
+    //   }
+    // };
+      
+  } catch (error) {
+    console.log("WebSocket Error:", error);
+  }
+  
 }
