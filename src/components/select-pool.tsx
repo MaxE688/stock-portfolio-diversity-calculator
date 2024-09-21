1a0f4a18fe86522e0cc00e0f7eed1774dac7606e
MaxE688 <max.evers8@gmail.com>
2024-09-21 19:53:23 -0400

    Set up action to play when waiting for search results

--- src/components/select-pool.tsx
+++ src/components/select-pool.tsx
@@ -11,17 +11,24 @@ interface Props {
 export default function SelectPool({ pool, setPool, handleNewStock }: Props) {
 
   const [ suggestionData, setSuggestionData ] = useState<Array<any>>([]);
+  const [ isWaiting, setIsWaiting ] = useState(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleOptionSelection = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setPool(e.target.value);
   }
 
   const handleQuery = useDebouncedCallback( async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const q = `http://localhost:3000/suggestions?` + new URLSearchParams({stock: String(e.target.value)}).toString();
-    const fetched = await fetch(q);
-    const responseData = await fetched.json();
-    setSuggestionData(JSON.parse(responseData));
-
+    if(e.target.value !== ""){
+      setIsWaiting(true);
+      const q = `http://localhost:3000/suggestions?` + new URLSearchParams({stock: String(e.target.value)}).toString();
+      const fetched = await fetch(q);
+      const responseData = await fetched.json();
+      setSuggestionData(JSON.parse(responseData));
+      setIsWaiting(false)
+    }
+    else{
+      setSuggestionData([]);
+    }
   }, 500);
 
   const getStockData = async (data: any, event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -38,13 +45,13 @@ export default function SelectPool({ pool, setPool, handleNewStock }: Props) {
   return(
     <div className="select-pool-container">
       {/* <form action="https://one-off-backends.onrender.com" method="post"> */}
-        <select onChange={handleChange}>
+        <select onChange={handleOptionSelection}>
           <option value="DOW 30">DOW 30</option>
           <option value="Custom List">Custom</option>
         </select>
         <div className={(pool === "DOW 30"? "hidden" : "block") + " search"}>
           <input placeholder="Search Symbol" onChange={handleQuery}></input>
-          <div className="query-suggestions">
+          <div className={"query-suggestions " + (isWaiting? "waiting" : "")}>
             {
               suggestionData.length > 0?
                 suggestionData.map( d => (
