d60dca3b9003b11ea8b5a3febddfa0956679723f
MaxE688 <max.evers8@gmail.com>
2024-11-12 17:41:23 -0500

    create table component

---
+++ src/components/table.tsx
@@ -0,0 +1,41 @@
+import { StockData } from "../lib/definitions"
+import TableRow from "./table-row"
+
+interface Props {
+  stocks: Array<StockData> | undefined,
+  selectedStocks: Array<StockData>,
+  stockPool: string,
+  handleClick: (stock: StockData) => void
+}
+
+export default function Table({ stocks, selectedStocks, stockPool, handleClick }: Props){
+  
+  // console.log("");
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Symbol</th>
+          <th>Current Price</th>
+          <th>Sector</th>
+          <th>{stockPool === "Custom List"? "Remove" : " "}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          stocks && stocks.map((stock, i) => (
+            
+            <TableRow
+              key={i} 
+              stock={stock} 
+              handleClick={handleClick} 
+              stockPool={stockPool}                
+              isSelected={ selectedStocks.includes(stock) } 
+            />
+          ))
+        }
+      </tbody>
+    </table>
+  )
+}
