a4558f64257520ece3e5d3ea2adc4c53c0401d9e
MaxE688 <max.evers8@gmail.com>
2024-11-12 17:42:06 -0500

    pagify new table component

--- src/components/all-stocks.tsx
+++ src/components/all-stocks.tsx
@@ -1,60 +1,113 @@
 
+import { useEffect, useState } from "react";
 import { StockData } from "../lib/definitions";
-import StockCard from "./stock-card";
-import TableRow from "./table-row";
+import Table from "./table";
 
 interface Props {
-  stocks: Array<StockData> | undefined,
+  stocks: Array<StockData>,
   selectedStocks: Array<StockData>,
   stockPool: string,
   handleClick: (stock: StockData) => void
 }
 
-
 export default function AllStocks({ stocks, selectedStocks, stockPool, handleClick }: Props){
+
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+  const [dataToDisplay, setDataToDisplay] = useState<StockData[]>([]);
+
+  const TOTAL_STOCKS_PER_PAGE = 10;
+  const totalPages = Math.ceil(stocks.length / TOTAL_STOCKS_PER_PAGE);
+
+  useEffect(() => {
+
+    setDataToDisplay(stocks?.slice(0, TOTAL_STOCKS_PER_PAGE));
+
+  }, []);
+
+  useEffect(() => {
+
+    const start = (currentPageNumber-1) * TOTAL_STOCKS_PER_PAGE;
+    const end = currentPageNumber * TOTAL_STOCKS_PER_PAGE;
+    setDataToDisplay(stocks.slice(start, end));
+
+  }, [currentPageNumber]);
+
+  const handlePageChange = (direction: 'left' | 'right') => {
+
+    let newPage = currentPageNumber;
+    if(direction === 'left' && currentPageNumber > 1){
+      newPage--;
+    }
+    else if(direction === 'right' && currentPageNumber < totalPages){
+      newPage++;
+    }
+
+    setCurrentPageNumber(newPage);
+  }
+
+
+  // console.log("breakpoint")
+
 
   return(
     <>
       <h3>{stockPool}</h3>
-      {/* <div className="all-stocks container">
-        { // if there are stocks to display
-          stocks && stocks.map((item, i) => (
-            <StockCard 
-              key={i} 
-              stock={item}
-              isSelected={ selectedStocks.includes(item) } 
-              handleClick={handleClick} 
-            />
-            
-          )) 
-        }
-      </div> */}
-      
-      {/* create table row component for stocks */}
-      <table>
-        <thead>
-          <tr>
-            <th>Symbol</th>
-            <th>Current Price</th>
-            <th>Sector</th>
-            <th>{stockPool === "Custom List"? "Remove" : " "}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            stocks && stocks.map((stock, i) => (
-             
-              <TableRow
-                key={i} 
-                stock={stock} 
-                handleClick={handleClick} 
-                stockPool={stockPool}                
-                isSelected={ selectedStocks.includes(stock) } 
-              />
-            ))
-          }
-        </tbody>
-      </table>
+      <Table 
+        stocks={dataToDisplay}
+        selectedStocks={selectedStocks}
+        stockPool={stockPool}
+        handleClick={handleClick}
+      />
+      <div className='page-controls'>
+        <PageArrow 
+          handleClick={handlePageChange}
+          direction="left"
+          isDisabled={currentPageNumber <= 1? true : false}
+        />
+        <PageNumber 
+          currentPage={currentPageNumber}
+          totalPages={totalPages}
+        />
+        <PageArrow 
+          handleClick={handlePageChange}
+          direction="right"
+          isDisabled={currentPageNumber >= totalPages? true : false}
+        />
+      </div>
     </>
+  );
+}
+
+
+
+interface PageArrowProps {
+  handleClick: (d: 'left' |'right') => void,
+  direction: 'left' | 'right',
+  isDisabled: boolean
+}
+
+function PageArrow({handleClick, direction, isDisabled}: PageArrowProps){
+
+  const icon = direction === 'left'? '<-' : '->';
+
+  return (
+    <button className={`arrow-${direction} ` + (isDisabled? `disabled-arrow`: ` `)} onClick={() => handleClick(direction)} >
+      {icon}
+    </button>
+  );
+
+}
+
+interface PageNumberProps {
+  currentPage: number,
+  totalPages: number
+}
+
+function PageNumber({ currentPage, totalPages }: PageNumberProps){
+
+  return(
+    <p>
+      {currentPage + " / " + totalPages}
+    </p>
   );
 }
