9bc0fed3d14b00253b9747bbe6ad0de1e4365516
MaxE688 <max.evers8@gmail.com>
2024-11-18 13:59:52 -0500

    rename component

--- src/components/portfolio-calculator.tsx
+++ src/components/portfolio-calculator.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { StockCardData, StockData, TradeInfo, quoteData } from "../lib/definitions";
 import AllStocks from "./all-stocks";
 import SelectedStocks from "./selected-stocks";
-import DiversityCalculator from "./score-components/diversity-calculator";
+import PortfolioData from "./score-components/portfolio-data";
 import { openWebSocket } from "../lib/ws-operations";
 import SelectPool from "./select-pool";
 
@@ -206,7 +206,7 @@ export default function PortfolioCalculator(){
             />
           </div>
           <div className="calculator-container">
-            <DiversityCalculator stocks={selectedStocks} />
+            <PortfolioData stocks={selectedStocks} />
           </div>
         </div>
         {
