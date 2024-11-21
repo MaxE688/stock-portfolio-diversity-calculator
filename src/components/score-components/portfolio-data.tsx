2d31a7f242c43f2b5224c833f5ad5f9106512ce4
MaxE688 <max.evers8@gmail.com>
2024-11-21 16:45:38 -0500

    Add new metrics

--- src/components/score-components/portfolio-data.tsx
+++ src/components/score-components/portfolio-data.tsx
@@ -1,5 +1,6 @@
 import { StockCardData } from "../../lib/definitions";
 import DiversityScore from "./diversity-score";
+import PortfolioAverage from "./portfolio-average";
 import PortfolioValue from "./portfolio-value";
 
 interface Props {
@@ -14,8 +15,15 @@ export default function PortfolioData({ stocks }: Props ) {
   
   return (
     <>
-      <DiversityScore stocks={stocks} />
-      <PortfolioValue stocks={stocks} />
+      <div>
+        <DiversityScore stocks={stocks} />
+      </div>
+      <div>
+        <PortfolioValue stocks={stocks} />
+      </div>
+      <div>
+        <PortfolioAverage stocks={stocks} />
+      </div>
     </>
     
   );
