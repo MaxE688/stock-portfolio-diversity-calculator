63527401c73fbf6f599351bdf4be03ae6792c36c
MaxE688 <max.evers8@gmail.com>
2024-11-22 16:42:56 -0500

    Add bar graph

--- src/components/industries.tsx
+++ src/components/industries.tsx
@@ -7,27 +7,46 @@ interface Props {
   stocks: Array<StockCardData>
 }
 
+interface RenderProps {
+  cx:number,
+  cy:number,
+  midAngle: number,
+  innerRadius: number,
+  outerRadius: number,
+  percent: number,
+  index: number
+}
+
 export default function Industries({ stocks }:Props){
 
   const s = getSectors(stocks);
   const [ sectors, setSectors ] = useState(s);
-  // const [ width, setWidth] = useState(480);
+  const [ barGraphWidth, setBarGraphWidth] = useState(500);
   // const [ height, setHeight ] = useState(180)
   const height = 400;
   const width = 500;
 
   useEffect(() => {
-    setSectors(getSectors(stocks));
+    const sectors = getSectors(stocks)
+    setSectors(sectors);
+    if(sectors.length > 3){
+      const barWidth = 135;
+      let w = 0;
+      for(let _ in sectors){
+        w += barWidth;
+      }
+      setBarGraphWidth(w);
+    }
+    else{
+      setBarGraphWidth(width);
+    }
   }, [stocks]);
 
-  // useEffect(() => {
-  //   setWidth(sectors.length * 160);
-  // }, [sectors]);
 
   const COLORS = ['#45322E', '#A5A5A5', '#CAC4B0', '#343B29', '#A65E2E', '#424632', '#C2B078', '#CB2821', '#317F43', '#5B3A29'];
 
   const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }: RenderProps) => {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
@@ -46,53 +65,34 @@ export default function Industries({ stocks }:Props){
     <>
       <div className='industry-container'>
       <h2 id='pie-chart-title'>Sectors</h2>
-        {/* <div>
-          <BarChart width={width} height={height} data={sectors}>
-            <CartesianGrid strokeDasharray="1" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Legend /> 
-            <Bar dataKey="value" fill="#8884d8" />
-            <Bar dataKey="weight" fill="#82ca9d" />
-          </BarChart>
-        </div> */}
         <div className="chart-container">
-          {/* <BarChart width={width} height={height} data={sectors}>
+          <PieChart width={width} height={height}>
+            <Pie
+              data={sectors}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={150}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {sectors.map((_entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Legend align="center" />
+          </PieChart>
+        </div>
+        <div className="bar-graph">
+          <BarChart width={barGraphWidth} height={height} data={sectors}>
             <CartesianGrid strokeDasharray="1" />
             <XAxis dataKey="name"  />
             <YAxis />
             <Legend /> 
             <Bar dataKey="value" fill="#8884d8" barSize={60}/>
-          </BarChart> */}
-          <PieChart width={width} height={height}>
-          <Pie
-            data={sectors}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={150}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {sectors.map((_entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-          <Legend align="center" />
-        </PieChart>
+          </BarChart>
         </div>
-        
-        
-        {/* {
-          sectors.map((sect, i) => (
-            <div key={i} className="industry-card">
-              <p className="symbol">{sect.name}</p>
-              <p className={` price`}>${Number(sect.value).toFixed(2)}</p>
-              <p className="sector">{Number(sect.weight * 100).toFixed(2) + '%'}</p>
-            </div>
-          ))
-        } */}
       </div>
     </>
   );
